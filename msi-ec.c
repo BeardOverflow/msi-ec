d59156af9aadbb959c0ff1777e850a5097d719dd
José Ángel Pastrana Padilla <japp0005@red.ujaen.es>
2021-08-28 01:51:43 +0200

    Added procfs interface and added a first entry to switch webcam on/off

--- msi-ec.c
+++ msi-ec.c
@@ -5,12 +5,119 @@
 #include <linux/init.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
+#include <linux/proc_fs.h>
+#include <linux/seq_file.h>
 
+#define MSI_PROC_DIR "msi-ec"
+#define MSI_PROC_VERSION 1
+#define MSI_EC_SWITCH_WEBCAM 0x2e
+#define MSI_EC_SWITCH_WEBCAM_ON 0x4a
+#define MSI_EC_SWITCH_WEBCAM_OFF 0x48
 #define MSI_EC_CHARGE_CONTROL_ADDRESS 0xef
 #define MSI_EC_CHARGE_CONTROL_OFFSET_START 0x8a
 #define MSI_EC_CHARGE_CONTROL_OFFSET_END 0x80
 #define MSI_EC_CHARGE_CONTROL_RANGE_MIN 0x8a
 #define MSI_EC_CHARGE_CONTROL_RANGE_MAX 0xe4
+
+#define compare_strings(x, y) (strcmp(x, y) == 0 || strcmp(x, y "\n") == 0)
+
+static inline char* kstring(const char *buf, size_t count)
+{
+	char *kbuf = kmalloc(count + 1, GFP_KERNEL);
+	if (kbuf == NULL)
+		return NULL;
+
+	if (copy_from_user(kbuf, buf, count) > 0) {
+		kfree(kbuf);
+		return NULL;
+	}
+
+	kbuf[count] = 0;
+	return kbuf;
+}
+
+static struct proc_dir_entry *msi_proc_dir;
+
+static int __maybe_unused version_proc_show(struct seq_file *m, void *v)
+{
+	seq_printf(m, "driver: %s\n", MSI_PROC_DIR);
+	seq_printf(m, "version: %d\n", MSI_PROC_VERSION);
+	return 0;
+}
+
+static int webcam_proc_show(struct seq_file *m, void *v)
+{
+        u8 rdata;
+        int result;
+
+        result = ec_read(MSI_EC_SWITCH_WEBCAM, &rdata);
+        if (result < 0)
+                return result;
+
+	switch (rdata) {
+		case MSI_EC_SWITCH_WEBCAM_ON:
+			seq_printf(m, "%s\n", "on");
+			break;
+		case MSI_EC_SWITCH_WEBCAM_OFF:
+			seq_printf(m, "%s\n", "off");
+			break;
+		default:
+			seq_printf(m, "%s\n", "unknown");
+			break;
+	}
+
+	return 0;
+}
+
+static int webcam_proc_open(struct inode *inode, struct file *file)
+{
+	return single_open(file, webcam_proc_show, PDE_DATA(inode));
+}
+
+static ssize_t webcam_proc_write(struct file *file, const char __user *buf,
+			      size_t count, loff_t *pos)
+{
+	int result = -EINVAL;
+
+	char *rdata = kstring(buf, count);
+	if (rdata == NULL)
+		return -EFAULT;
+
+	if (compare_strings(rdata, "on"))
+        	result = ec_write(MSI_EC_SWITCH_WEBCAM, MSI_EC_SWITCH_WEBCAM_ON);
+
+	if (compare_strings(rdata, "off"))
+        	result = ec_write(MSI_EC_SWITCH_WEBCAM, MSI_EC_SWITCH_WEBCAM_OFF);
+
+	kfree(rdata);
+
+        if (result < 0)
+                return result;
+
+	return count;
+}
+
+static const struct proc_ops webcam_proc_ops = {
+	.proc_open	= webcam_proc_open,
+	.proc_read	= seq_read,
+	.proc_lseek	= seq_lseek,
+	.proc_release	= single_release,
+	.proc_write	= webcam_proc_write,
+};
+
+static void create_msi_proc_entries(void)
+{
+	proc_create_data("webcam", S_IRUGO | S_IWUSR, msi_proc_dir,
+				&webcam_proc_ops, NULL);
+	proc_create_single_data("version", S_IRUGO, msi_proc_dir,
+				version_proc_show, NULL);
+}
+
+static void remove_msi_proc_entries(void)
+{
+	remove_proc_entry("webcam", msi_proc_dir);
+	remove_proc_entry("version", msi_proc_dir);
+}
 
 static ssize_t charge_control_threshold_show(u8 offset,
 				struct device *device,
@@ -111,15 +218,33 @@ static struct acpi_battery_hook battery_hook = {
 
 static int __init msi_ec_module_init(void)
 {
+	if (acpi_disabled) {
+		pr_err("ACPI needs to be enabled for this driver to work!\n");
+		return -ENODEV;
+	}
+
+	msi_proc_dir = proc_mkdir(MSI_PROC_DIR, acpi_root_dir);
+	if (!msi_proc_dir) {
+		pr_err("Unable to create proc dir " MSI_PROC_DIR "\n");
+		return -ENODEV;
+	}
+	create_msi_proc_entries();
+
 	pr_info("msi-ec: module_init\n");
 	battery_hook_register(&battery_hook);
+
 	return 0;
 }
 
 static void __exit msi_ec_module_exit(void)
 {
 	pr_info("msi-ec: module_exit\n");
+
 	battery_hook_unregister(&battery_hook);
+
+	remove_msi_proc_entries();
+	if (msi_proc_dir)
+		remove_proc_entry(MSI_PROC_DIR, acpi_root_dir);
 }
 
 MODULE_LICENSE("GPL");
