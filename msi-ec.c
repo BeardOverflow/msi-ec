7ad3d4afef1c4c2a2aea51af0c37b9f71eefcbfa
mutchiko <95985922+mutchiko@users.noreply.github.com>
2024-05-19 21:22:35 +0200

    Update msi-ec.c
    support for Alpha 17 b5eek

--- msi-ec.c
+++ msi-ec.c
@@ -1637,6 +1637,84 @@ static struct msi_ec_conf CONF19 __initdata = {
 	},
 };
 
+static const char *ALLOWED_FW_20[] __initconst = {
+	"17LLEMS1.106", // Alpha 17 B5EEK
+	NULL
+};
+
+static struct msi_ec_conf CONF20 __initdata = {
+	.allowed_fw = ALLOWED_FW_20,
+	.charge_control = {
+		.address      = 0xef,
+		.offset_start = 0x8a,
+		.offset_end   = 0x80,
+		.range_min    = 0x8a,
+		.range_max    = 0xe4,
+	},
+	.webcam = {
+		.address       = 0x2e,
+		.block_address = 0x2f,
+		.bit           = 1,
+	},
+	.fn_win_swap = {
+		.address = 0xbf,
+		.bit     = 4,
+	},
+	.cooler_boost = {
+		.address = 0x98,
+		.bit     = 7,
+	},
+	.shift_mode = {
+		.address = 0xf2,
+		.modes = {
+			{ SM_ECO_NAME,     0xc2 }, // super_battery = 0xa5
+			{ SM_COMFORT_NAME, 0xc1 }, // super_battery = 0xa4
+			{ SM_SPORT_NAME,   0xc1 }, // super_battery = 0xa1
+			{ SM_TURBO_NAME,   0xc4 }, // super_battery = 0xa0
+			MSI_EC_MODE_NULL
+		},
+	},
+	.super_battery = {
+		.address = MSI_EC_ADDR_UNKNOWN, // knwon. 0xd5.
+		.mask    = 0x0f,
+	},
+	.fan_mode = {
+		.address = 0xf4,
+		.modes = {
+			{ FM_AUTO_NAME,     0x0d },
+			{ FM_SILENT_NAME,   0x1d },
+			{ FM_ADVANCED_NAME, 0x8d },
+			MSI_EC_MODE_NULL
+		},
+	},
+	.cpu = {
+		.rt_temp_address       = 0x68,
+		.rt_fan_speed_address  = 0x71,
+		.rt_fan_speed_base_min = 0x19,
+		.rt_fan_speed_base_max = 0x37,
+		.bs_fan_speed_address  = MSI_EC_ADDR_UNKNOWN,
+		.bs_fan_speed_base_min = 0x00,
+		.bs_fan_speed_base_max = 0x0f,
+	},
+	.gpu = {
+		.rt_temp_address      = 0x80,
+		.rt_fan_speed_address = 0x89,
+	},
+	.leds = {
+		.micmute_led_address = 0x2b,
+		.mute_led_address    = 0x2c,
+		.bit                 = 2,
+	},
+	.kbd_bl = {
+		.bl_mode_address  = MSI_EC_ADDR_UNKNOWN,
+		.bl_modes         = { 0x00, 0x08 },
+		.max_mode         = 1, // ?
+		.bl_state_address = 0xf3,
+		.state_base_value = 0x80,
+		.max_state        = 3,
+	},
+};
+
 static struct msi_ec_conf *CONFIGURATIONS[] __initdata = {
 	&CONF0,
 	&CONF1,
@@ -1658,6 +1736,7 @@ static struct msi_ec_conf *CONFIGURATIONS[] __initdata = {
 	&CONF17,
 	&CONF18,
 	&CONF19,
+	&CONF20,
 	NULL
 };
 
