e261feba4ae92d1626ed5a9c1ce89883b303524c
m1dugh <romain.le-miere@epita.fr>
2024-09-23 17:32:13 +0200

    feat(nix): adds development package

--- default.nix
+++ default.nix
@@ -1,48 +1,53 @@
-{ stdenv
-, lib
-, fetchFromGitHub
-, linuxPackages
-, kernel ? linuxPackages.kernel
-, ...
+{
+  stdenv,
+  lib,
+  fetchFromGitHub,
+  linuxPackages,
+  kernel ? linuxPackages.kernel,
+  development ? false,
+  ...
 }:
+let
+  fs = lib.fileset;
+  githubSrc = fetchFromGitHub {
+    owner = "BeardOverflow";
+    repo = "msi-ec";
+    rev = "e5820a2b415e796db9dfb204250f7410b6662ac2";
+    hash = "sha256-1nwIf5OWjJpLLRUKeSOcZ1yvBGE51rUAZLmZjkt8K04=";
+  };
+  localSrc = fs.union [
+    ./msi-ec.c
+    ./ec_memory_configuration.h
+  ];
+  src = if development then localSrc else githubSrc;
+in
 stdenv.mkDerivation rec {
-	pname = "msi-ec-kmods";
-	version = "2024-09-18";
+  pname = "msi-ec-kmods";
+  version = "2024-09-18";
 
-	src = fetchFromGitHub {
-		owner = "BeardOverflow";
-		repo = "msi-ec";
-		rev = "e5820a2b415e796db9dfb204250f7410b6662ac2";
-		hash = "sha256-1nwIf5OWjJpLLRUKeSOcZ1yvBGE51rUAZLmZjkt8K04=";
-	};
+  inherit src;
 
-	hardeningDisable = [
-		"pic"
-	];
+  hardeningDisable = [ "pic" ];
 
-	makeFlags = kernel.makeFlags ++ [
-		"INSTALL_MOD_PATH=${placeholder "out"}"
-	];
+  makeFlags = kernel.makeFlags ++ [ "INSTALL_MOD_PATH=${placeholder "out"}" ];
 
-	KSRC = "${kernel.dev}/lib/modules/${kernel.modDirVersion}/build";
+  KSRC = "${kernel.dev}/lib/modules/${kernel.modDirVersion}/build";
 
-	nativeBuildInputs = kernel.moduleBuildDependencies;
+  nativeBuildInputs = kernel.moduleBuildDependencies;
 
-	buildPhase = ''
-		make -C ${KSRC} modules "M=$(pwd -P)"
-	'';
+  buildPhase = "	make -C ${KSRC} modules \"M=$(pwd -P)\"\n";
 
-	installPhase = ''
-		make -C ${KSRC} M=$(pwd) modules_install $makeFlags
-		'';
+  installPhase = ''
+    make -C ${KSRC} M=$(pwd) modules_install $makeFlags
+  '';
 
-	enableParallelBuilding = true;
+  enableParallelBuilding = true;
 
-	meta = with lib; {
-		description = "Kernel modules for MSI Embedded controller";
-		homepage = "https://github.com/BeardOverflow/msi-ec";
-		license = licenses.gpl3Only;
-		maintainers = [];
-		platforms = platforms.linux;
-	};
+  meta = with lib; {
+    description = "Kernel modules for MSI Embedded controller";
+    homepage = "https://github.com/BeardOverflow/msi-ec";
+    license = licenses.gpl3Only;
+    maintainers = [ ];
+    platforms = platforms.linux;
+  };
 }
--- flake.nix
+++ flake.nix
@@ -5,17 +5,20 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
   };
 
-  outputs = { self, nixpkgs }: 
-  let
-	system = "x86_64-linux";
-	pkgs = nixpkgs.legacyPackages.${system};
-  in {
+  outputs =
+    { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
 
-	  packages.${system} = rec {
-		  msi-ec = pkgs.callPackage ./default.nix {
-			  inherit pkgs;
-		  };
-		  default = msi-ec;
-	  };
-  };
+      packages.${system} = rec {
+        msi-ec = pkgs.callPackage ./default.nix { inherit pkgs; };
+        msi-ec-dev = msi-ec.overrideAttrs { development = true; };
+        default = msi-ec;
+      };
+
+      formatter.${system} = pkgs.nixfmt-rfc-style;
+    };
 }
