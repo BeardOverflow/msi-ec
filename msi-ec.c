c21572dfa5325317f962ebec2089f6471b207e7f
Aleksandr Gordeiko <graminit@gmail.com>
2025-01-13 22:20:02 +0300

    Add support for Prestige 14 Evo A12M (14C6EMS1.109)

--- msi-ec.c
+++ msi-ec.c
@@ -2783,6 +2783,80 @@ static struct msi_ec_conf CONF33 __initdata = {
 	},
 };
 
+static const char *ALLOWED_FW_34[] __initconst = {
+	"14C6EMS1.109", // Prestige 14 Evo A12M
+	NULL
+};
+
+static struct msi_ec_conf CONF34 __initdata = {
+	.allowed_fw = ALLOWED_FW_34,
+	.charge_control = {
+		.address      = 0xd7,
+		.offset_start = 0x8a,
+		.offset_end   = 0x80,
+		.range_min    = 0x8a,
+		.range_max    = 0xe4,
+	},
+	.webcam = {
+		.address       = 0x2e,
+		.block_address = 0x2f,
+		.bit           = 1,
+	},
+	.fn_win_swap = {
+		.address = 0xe8,
+		.bit     = 4,
+		.invert  = false,
+	},
+	.cooler_boost = {
+		.address = 0x98,
+		.bit     = 7,
+	},
+	.shift_mode = {
+		.address = 0xd2,
+		.modes = {
+			{ SM_ECO_NAME,     0xc2 }, // super battery
+			{ SM_COMFORT_NAME, 0xc1 }, // silent / balanced
+			{ SM_SPORT_NAME,   0xc0 }, // high performance
+			MSI_EC_MODE_NULL
+		},
+	},
+	.super_battery = {
+		.address = 0xeb,
+		.mask    = 0x0f,
+	},
+	.fan_mode = {
+		.address = 0xd4,
+		.modes = {
+			{ FM_AUTO_NAME,     0x0d }, // super battery, balanced and auto high performance modes
+			{ FM_SILENT_NAME,   0x1d }, // silent mode
+			{ FM_ADVANCED_NAME, 0x4d }, // advanced high performance mode
+			MSI_EC_MODE_NULL
+		},
+	},
+	.cpu = {
+		.rt_temp_address       = 0x68,
+		.rt_fan_speed_address  = MSI_EC_ADDR_UNKNOWN,
+		.bs_fan_speed_address  = MSI_EC_ADDR_UNKNOWN,
+	},
+	.gpu = {
+		.rt_temp_address      = MSI_EC_ADDR_UNKNOWN,
+		.rt_fan_speed_address = MSI_EC_ADDR_UNKNOWN,
+	},
+	.leds = {
+		.micmute_led_address = 0x2c,
+		.mute_led_address    = 0x2d,
+		.bit                 = 1,
+	},
+	.kbd_bl = {
+		.bl_mode_address  = 0x2c,
+		.bl_modes         = { 0x00, 0x08 }, // always on / off after 10 sec
+		.max_mode         = 1,
+		.bl_state_address = 0xd3,
+		.state_base_value = 0x80,
+		.max_state        = 3,
+	},
+};
+
 static struct msi_ec_conf *CONFIGURATIONS[] __initdata = {
 	&CONF0,
 	&CONF1,
@@ -2818,6 +2892,7 @@ static struct msi_ec_conf *CONFIGURATIONS[] __initdata = {
 	&CONF31,
 	&CONF32,
 	&CONF33,
+	&CONF34,
 	NULL
 };
 
