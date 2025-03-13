7cf1acf60255f563f72a3f2953283214985b5356
SnikersQD <87077512+SnikersQD@users.noreply.github.com>
2025-03-13 23:18:34 +0200

    Add support for Modern 14 B4MW

--- msi-ec.c
+++ msi-ec.c
@@ -3234,6 +3234,73 @@ static struct msi_ec_conf CONF42 __initdata = {
 	},
 };
 
+static const char *ALLOWED_FW_43[] __initconst = {
+	"14DKEMS1.104", // Modern 14 B4MW
+	NULL
+};
+
+static struct msi_ec_conf CONF43 __initdata = {
+	.allowed_fw = ALLOWED_FW_43, // WMI1 based
+	.charge_control_address = 0xef,
+	.webcam = {
+		.address       = 0x2e,
+		.block_address = 0x2f,
+		.bit           = 1,
+	},
+	.fn_win_swap = {
+		.address = 0xbf,
+		.bit     = 4,
+		.invert  = false,
+	},
+	.cooler_boost = {
+		.address = 0x98,
+		.bit     = 7,
+	},
+	.shift_mode = {
+		.address = 0xf2,
+		.modes = {
+			{ SM_ECO_NAME,     0xc2 }, // Super Battery
+			{ SM_COMFORT_NAME, 0xc1 }, // Silent / Balanced / AI
+			{ SM_SPORT_NAME,   0xc0 }, // Performance
+			MSI_EC_MODE_NULL
+		},
+	},
+	.super_battery = {
+		.address = MSI_EC_ADDR_UNSUPP, // 0x33 switches between 0x0D and 0x05
+		.mask    = 0x0f,
+	},
+	.fan_mode = {
+		.address = 0xd4,
+		.modes = {
+			{ FM_AUTO_NAME,     0x0d },
+			{ FM_SILENT_NAME,   0x1d },
+			{ FM_ADVANCED_NAME, 0x8d },
+			MSI_EC_MODE_NULL
+		},
+	},
+	.cpu = {
+		.rt_temp_address      = 0x68,
+		.rt_fan_speed_address = 0x71,
+	},
+	.gpu = {
+		.rt_temp_address      = MSI_EC_ADDR_UNSUPP,
+		.rt_fan_speed_address = MSI_EC_ADDR_UNSUPP,
+	},
+	.leds = {
+		.micmute_led_address = 0x2b,
+		.mute_led_address    = 0x2c,
+		.bit                 = 2,
+	},
+	.kbd_bl = {
+		.bl_mode_address  = MSI_EC_ADDR_UNSUPP, // not presented in MSI app
+		.bl_modes         = { 0x00, 0x08 },
+		.max_mode         = 1,
+		.bl_state_address = 0xf3,
+		.state_base_value = 0x80,
+		.max_state        = 3,
+	},
+};
+
 static struct msi_ec_conf *CONFIGURATIONS[] __initdata = {
 	&CONF0,
 	&CONF1,
@@ -3278,6 +3345,7 @@ static struct msi_ec_conf *CONFIGURATIONS[] __initdata = {
 	&CONF40,
 	&CONF41,
 	&CONF42,
+	&CONF43,
 	NULL
 };
 
