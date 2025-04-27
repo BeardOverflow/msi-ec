7f9e0f23647e5507936029b75ebe3563b18a7a35
Ahmed AbdelHamid <a.hamid@virtasant.com>
2025-04-27 22:52:16 +0300

    fix fan advanced mode

--- msi-ec.c
+++ msi-ec.c
@@ -3205,7 +3205,7 @@ static struct msi_ec_conf CONF45 __initdata = {
 		.modes = {
 			{ FM_AUTO_NAME,     0x0d },
 			{ FM_SILENT_NAME,   0x1d },
-			{ FM_ADVANCED_NAME, 0x4d },
+			{ FM_ADVANCED_NAME, 0x8d },
 			MSI_EC_MODE_NULL
 		},
 	},
