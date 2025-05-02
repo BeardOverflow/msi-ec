8979ad35160dba2c4ca6cad41c7b45e39e56fc5d
GlitchPunk <16731610+glpnk@users.noreply.github.com>
2025-05-02 21:11:46 +0300

    add `Katana GF66 12UC` `1584EMS1.112`

--- msi-ec.c
+++ msi-ec.c
@@ -3512,6 +3512,73 @@ static struct msi_ec_conf CONF49 __initdata = {
 	},
 };
 
+static const char *ALLOWED_FW_50[] __initconst = {
+	"1584EMS1.112", // Katana GF66 12UC
+	NULL
+};
+
+static struct msi_ec_conf CONF50 __initdata = {
+	.allowed_fw = ALLOWED_FW_50, // WMI2 based
+	.charge_control_address = 0xd7,
+	.webcam = {
+		.address       = 0x2e,
+		.block_address = MSI_EC_ADDR_UNSUPP,
+		.bit           = 1,
+	},
+	.fn_win_swap = {
+		.address = 0xe8,
+		.bit     = 4,
+		.invert  = true,
+	},
+	.cooler_boost = {
+		.address = 0x98,
+		.bit     = 7,
+	},
+	.shift_mode = {
+		.address = 0xd2,
+		.modes = {
+			{ SM_ECO_NAME,     0xc2 }, // super battery (D4=0D)
+			{ SM_COMFORT_NAME, 0xc1 }, // balanced + silent
+			{ SM_TURBO_NAME,   0xc4 }, // extreme performance
+			MSI_EC_MODE_NULL
+		},
+	},
+	.super_battery = {
+		.address = 0xeb, // only in SB
+		.mask    = 0x0f,
+	},
+	.fan_mode = {
+		.address = 0xd4,
+		.modes = {
+			{ FM_AUTO_NAME,     0x0d },
+			{ FM_SILENT_NAME,   0x1d }, // only silent
+			{ FM_ADVANCED_NAME, 0x8d },
+			MSI_EC_MODE_NULL
+		},
+	},
+	.cpu = {
+		.rt_temp_address      = 0x68,
+		.rt_fan_speed_address = 0x71,
+	},
+	.gpu = {
+		.rt_temp_address      = 0x80,
+		.rt_fan_speed_address = 0x89,
+	},
+	.leds = {
+		.micmute_led_address = 0x2c,
+		.mute_led_address    = 0x2d,
+		.bit                 = 1,
+	},
+	.kbd_bl = {
+		.bl_mode_address  = MSI_EC_ADDR_UNSUPP,
+		.bl_modes         = { 0x00, 0x08 },
+		.max_mode         = 1,
+		.bl_state_address = 0xd3,
+		.state_base_value = 0x80,
+		.max_state        = 3,
+	},
+};
+
 static struct msi_ec_conf *CONFIGURATIONS[] __initdata = {
 	&CONF0,
 	&CONF1,
@@ -3563,6 +3630,7 @@ static struct msi_ec_conf *CONFIGURATIONS[] __initdata = {
 	&CONF47,
 	&CONF48,
 	&CONF49,
+	&CONF50,
 	NULL
 };
 
