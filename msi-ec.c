5d88af783947f8e344d3e43a38fcce9c5ed807dd
mutchiko <95985922+mutchiko@users.noreply.github.com>
2025-05-09 20:57:29 +0200

    Add Bravo 15 B5ED support

--- msi-ec.c
+++ msi-ec.c
@@ -3579,6 +3579,73 @@ static struct msi_ec_conf CONF50 __initdata = {
 	},
 };
 
+static const char *ALLOWED_FW_51[] __initconst = {
+	"158MEMS1.101", // Bravo 15 B5ED
+	NULL
+};
+
+static struct msi_ec_conf CONF51 __initdata = {
+	.allowed_fw = ALLOWED_FW_51, // WMI1 based
+	.charge_control_address = 0xef,
+	.webcam = {
+		.address       = 0x2e,
+		.block_address = 0x2f,
+		.bit           = 1,
+	},
+	.fn_win_swap = {
+		.address = 0xbf,
+		.bit     = 4,
+		.invert  = true,
+	},
+	.cooler_boost = {
+		.address = 0x98,
+		.bit     = 7,
+	},
+	.shift_mode = {
+		.address = 0xf2,
+		.modes = {
+			{ SM_ECO_NAME,     0xc2 },
+			{ SM_SPORT_NAME,   0xc1 },
+			{ SM_TURBO_NAME,   0xc4 },
+			MSI_EC_MODE_NULL
+		},
+	},
+	.super_battery = {
+		.address = MSI_EC_ADDR_UNKNOWN, // 0xd5 (automatic switching with shift mode)
+		.mask    = 0x0f,
+	},
+	.fan_mode = {
+		.address = 0xf4,
+		.modes = {
+			{ FM_AUTO_NAME,     0x0d },
+			{ FM_SILENT_NAME,   0x1d },
+			{ FM_ADVANCED_NAME, 0x8d },
+			MSI_EC_MODE_NULL
+		},
+	},
+	.cpu = {
+		.rt_temp_address      = 0x68,
+		.rt_fan_speed_address = 0x71,
+	},
+	.gpu = {
+		.rt_temp_address      = 0x80,
+		.rt_fan_speed_address = 0x89,
+	},
+	.leds = {
+		.micmute_led_address = 0x2b,
+		.mute_led_address    = 0x2c,
+		.bit                 = 2,
+	},
+	.kbd_bl = {
+		.bl_mode_address  = MSI_EC_ADDR_UNKNOWN,
+		.bl_modes         = { 0x00, 0x08 },
+		.max_mode         = 1,
+		.bl_state_address = 0xf3,
+		.state_base_value = 0x80,
+		.max_state        = 3,
+	},
+};
+
 static struct msi_ec_conf *CONFIGURATIONS[] __initdata = {
 	&CONF0,
 	&CONF1,
@@ -3631,6 +3698,7 @@ static struct msi_ec_conf *CONFIGURATIONS[] __initdata = {
 	&CONF48,
 	&CONF49,
 	&CONF50,
+	&CONF51,
 	NULL
 };
 
