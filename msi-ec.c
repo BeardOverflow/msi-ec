8fcd9d9532819b5002ab0586133b2c0f26e9f9ed
mutchiko <95985922+mutchiko@users.noreply.github.com>
2025-05-10 23:18:32 +0200

    Add support for Prestige 14 Evo - B13M

--- msi-ec.c
+++ msi-ec.c
@@ -3646,6 +3646,73 @@ static struct msi_ec_conf CONF51 __initdata = {
 	},
 };
 
+static const char *ALLOWED_FW_52[] __initconst = {
+	"14F1EMS1.207", // Prestige 14 Evo - B13M
+	NULL
+};
+
+static struct msi_ec_conf CONF52 __initdata = {
+	.allowed_fw = ALLOWED_FW_52, // WMI2 based
+	.charge_control_address = 0xd7,
+	.webcam = {
+		.address       = 0x2e,
+		.block_address = MSI_EC_ADDR_UNSUPP,
+		.bit           = 1,
+	},
+	.fn_win_swap = {
+		.address = 0xe8,
+		.bit     = 4,
+		.invert  = false,
+	},
+	.cooler_boost = {
+		.address = 0x98,
+		.bit     = 7,
+	},
+	.shift_mode = {
+		.address = 0xd2,
+		.modes = {
+			{ SM_ECO_NAME,     0xc2 }, // super battery
+			{ SM_COMFORT_NAME, 0xc1 }, // silent / balanced
+			{ SM_TURBO_NAME,   0xc4 }, // high performance
+			MSI_EC_MODE_NULL
+		},
+	},
+	.super_battery = {
+		.address = 0xeb,
+		.mask    = 0x0f,
+	},
+	.fan_mode = {
+		.address = 0xd4,
+		.modes = {
+			{ FM_AUTO_NAME,     0x0d }, // super battery, balanced and auto high performance modes
+			{ FM_SILENT_NAME,   0x1d }, // silent mode
+			{ FM_ADVANCED_NAME, 0x8d }, // advanced high performance mode
+			MSI_EC_MODE_NULL
+		},
+	},
+	.cpu = {
+		.rt_temp_address      = 0x68,
+		.rt_fan_speed_address = 0x71,
+	},
+	.gpu = {
+		.rt_temp_address      = MSI_EC_ADDR_UNKNOWN,
+		.rt_fan_speed_address = MSI_EC_ADDR_UNKNOWN,
+	},
+	.leds = {
+		.micmute_led_address = 0x2c,
+		.mute_led_address    = 0x2d,
+		.bit                 = 1,
+	},
+	.kbd_bl = {
+		.bl_mode_address  = 0x2c,
+		.bl_modes         = { 0x00, 0x08 }, // always on / off after 10 sec
+		.max_mode         = 1,
+		.bl_state_address = 0xd3,
+		.state_base_value = 0x80,
+		.max_state        = 3,
+	},
+};
+
 static struct msi_ec_conf *CONFIGURATIONS[] __initdata = {
 	&CONF0,
 	&CONF1,
@@ -3699,6 +3766,7 @@ static struct msi_ec_conf *CONFIGURATIONS[] __initdata = {
 	&CONF49,
 	&CONF50,
 	&CONF51,
+	&CONF52,
 	NULL
 };
 
