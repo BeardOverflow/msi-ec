0d37452dfa4cf4a57a09892c696e232a660d5e1b
mutchiko <95985922+mutchiko@users.noreply.github.com>
2025-06-01 09:17:35 +0200

    add  Stealth A16 AI+ A3XVFG

--- msi-ec.c
+++ msi-ec.c
@@ -3787,6 +3787,73 @@ static struct msi_ec_conf CONF53 __initdata = {
 	},
 };
 
+static const char *ALLOWED_FW_54[] __initconst = {
+	"15FKIMS1.106", // Stealth A16 AI+ A3XVFG
+	NULL
+};
+
+static struct msi_ec_conf CONF54 __initdata = {
+	.allowed_fw = ALLOWED_FW_54, // WMI2 based
+	.charge_control_address = 0xd7,
+	.webcam = {
+		.address       = 0x2e,
+		.block_address = MSI_EC_ADDR_UNSUPP,
+		.bit           = 1,
+	},
+	.fn_win_swap = {
+		.address = 0xe8,
+		.bit     = 4,
+		.invert  = true,
+	},
+	.cooler_boost = {
+		.address = 0x98,
+		.bit     = 7,
+	},
+	.shift_mode = {
+		.address = 0xD2,
+		.modes = {
+			{ SM_ECO_NAME,     0xc2 },
+			{ SM_COMFORT_NAME, 0xc1 },
+			{ SM_TURBO_NAME,   0xc4 },
+			MSI_EC_MODE_NULL
+		},
+	},
+	.super_battery = {
+		.address = 0xeb,
+		.mask    = 0x0f,
+	},
+	.fan_mode = {
+		.address = 0xd4,
+		.modes = {
+			{ FM_AUTO_NAME,     0x0d },
+			{ FM_SILENT_NAME,   0x1d },
+			{ FM_ADVANCED_NAME, 0x8d },
+			MSI_EC_MODE_NULL
+		},
+	},
+	.cpu = {
+		.rt_temp_address      = 0x68,
+		.rt_fan_speed_address = 0x71,
+	},
+	.gpu = {
+		.rt_temp_address      = 0x80,
+		.rt_fan_speed_address = 0x89,
+	},
+	.leds = {
+		.micmute_led_address = 0x2c,
+		.mute_led_address    = 0x2d,
+		.bit                 = 1,
+	},
+	.kbd_bl = {
+		.bl_mode_address  = MSI_EC_ADDR_UNSUPP,
+		.bl_modes         = { 0x00, 0x08 },
+		.max_mode         = 1,
+		.bl_state_address = MSI_EC_ADDR_UNSUPP,
+		.state_base_value = 0x80,
+		.max_state        = 3,
+	},
+};
+
 static struct msi_ec_conf *CONFIGURATIONS[] __initdata = {
 	&CONF0,
 	&CONF1,
@@ -3842,6 +3909,7 @@ static struct msi_ec_conf *CONFIGURATIONS[] __initdata = {
 	&CONF51,
 	&CONF52,
 	&CONF53,
+	&CONF54,
 	NULL
 };
 
