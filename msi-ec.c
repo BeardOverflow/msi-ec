bcf75bf7453f9855e736181078e64fc56f0c0775
aishen944 <aishen944@163.com>
2025-08-14 20:52:35 +0800

    Support for Vector A18 HX A9WHG (#428)
    Support for Vector A18 HX A9WHG

--- msi-ec.c
+++ msi-ec.c
@@ -3954,6 +3954,69 @@ static struct msi_ec_conf CONF55 __initdata = {
 	},
 };
 
+static const char *ALLOWED_FW_56[] __initconst = {
+	"182LIMS1.108", // Vector A18 HX A9WHG
+	NULL
+};
+
+static struct msi_ec_conf CONF56 __initdata = {
+	.allowed_fw = ALLOWED_FW_56,
+	.charge_control_address = 0xd7,
+	.webcam = {
+		.address       = MSI_EC_ADDR_UNSUPP,
+		.block_address = MSI_EC_ADDR_UNSUPP,
+		.bit           = 1,
+	},
+	.fn_win_swap = {
+		.address = 0xe8,
+		.bit     = 4,
+		.invert  = true,
+	},
+	.cooler_boost = {
+		.address = 0x98,
+		.bit     = 7,
+	},
+	.shift_mode = {
+		.address = 0xd2,
+		.modes = {
+			{ SM_ECO_NAME,     0xc2 },
+			{ SM_COMFORT_NAME, 0xc1 },
+			{ SM_TURBO_NAME,   0xc4 },
+			MSI_EC_MODE_NULL
+		},
+	},
+	.super_battery = {
+		.address = MSI_EC_ADDR_UNSUPP,
+	},
+	.fan_mode = {
+		.address = 0xd4,
+		.modes = {
+			{ FM_AUTO_NAME,     0x0d },
+			{ FM_ADVANCED_NAME, 0x8d }, 
+			MSI_EC_MODE_NULL
+		},
+	},
+	.cpu = {
+		.rt_temp_address      = 0x68,
+		.rt_fan_speed_address = 0x71,
+	},
+	.gpu = {
+		.rt_temp_address      = 0x80,
+		.rt_fan_speed_address = 0x89,
+	},
+	.leds = {
+		.micmute_led_address = 0x2c,
+		.mute_led_address    = 0x2d,
+		.bit                 = 1,
+	},
+	.kbd_bl = {
+		.bl_mode_address  = MSI_EC_ADDR_UNSUPP,
+		.bl_state_address = MSI_EC_ADDR_UNSUPP,	// Can see 0xd3's value was changed, but not effected
+		.state_base_value = 0x80,
+		.max_state        = 3,
+	},
+};
+
 static struct msi_ec_conf *CONFIGURATIONS[] __initdata = {
 	&CONF0,
 	&CONF1,
@@ -4011,6 +4074,7 @@ static struct msi_ec_conf *CONFIGURATIONS[] __initdata = {
 	&CONF53,
 	&CONF54,
 	&CONF55,
+	&CONF56,
 	NULL
 };
 
