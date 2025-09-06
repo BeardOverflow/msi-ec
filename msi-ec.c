b3729a0575bfd170f4501259f99a385ff2db67c3
GlitchPunk <16731610+glpnk@users.noreply.github.com>
2025-09-06 05:11:07 +0300

    move 38 to 1

--- msi-ec.c
+++ msi-ec.c
@@ -131,6 +131,7 @@ static const char *ALLOWED_FW_1[] __initconst = {
 	"17F2EMS1.107",
 	"17F3EMS2.103", // GF75 Thin 10SER
 	"17F4EMS2.100", // GF75 Thin 9SCSR
+	"17E8EMS1.101", // GL75 Leopard 10SCXR
 	NULL
 };
 
@@ -182,8 +183,8 @@ static struct msi_ec_conf CONF1 __initdata = {
 		.rt_fan_speed_address = 0x89,
 	},
 	.leds = {
-		.micmute_led_address = 0x2b,
-		.mute_led_address    = 0x2c,
+		.micmute_led_address = MSI_EC_ADDR_UNSUPP,
+		.mute_led_address    = MSI_EC_ADDR_UNSUPP,
 		.bit                 = 2,
 	},
 	.kbd_bl = {
@@ -2723,72 +2724,6 @@ static struct msi_ec_conf CONF37 __initdata = {
 	},
 };
 
-static const char *ALLOWED_FW_38[] __initconst = {
-	"17E8EMS1.101", // GL75 Leopard 10SCXR
-	NULL
-};
-
-static struct msi_ec_conf CONF38 __initdata = {
-	.allowed_fw = ALLOWED_FW_38, // WMI1 based
-	.charge_control_address = 0xef,
-	.webcam = {
-		.address       = 0x2e,
-		.block_address = 0x2f,
-		.bit           = 1,
-	},
-	.fn_win_swap = {
-		.address = 0xbf,
-		.bit     = 4,
-		.invert  = false,
-	},
-	.cooler_boost = {
-		.address = 0x98,
-		.bit     = 7,
-	},
-	.shift_mode = {
-		.address = 0xf2,
-		.modes = {
-			{ SM_ECO_NAME,     0xc2 },
-			{ SM_COMFORT_NAME, 0xc1 },
-			{ SM_SPORT_NAME,   0xc0 },
-			{ SM_TURBO_NAME,   0xc4 },
-			MSI_EC_MODE_NULL
-		},
-	},
-	.super_battery = {
-		.address = MSI_EC_ADDR_UNKNOWN,
-	},
-	.fan_mode = {
-		.address = 0xf4,
-		.modes = {
-			{ FM_AUTO_NAME,     0x00 },
-			{ FM_ADVANCED_NAME, 0x80 },
-			MSI_EC_MODE_NULL
-		},
-	},
-	.cpu = {
-		.rt_temp_address      = 0x68,
-		.rt_fan_speed_address = 0x71,
-	},
-	.gpu = {
-		.rt_temp_address      = 0x80,
-		.rt_fan_speed_address = 0x89,
-	},
-	.leds = {
-		.micmute_led_address = MSI_EC_ADDR_UNKNOWN,
-		.mute_led_address    = MSI_EC_ADDR_UNKNOWN,
-		.bit                 = 1,
-	},
-	.kbd_bl = {
-		.bl_mode_address  = 0x2c,
-		.bl_modes         = { 0x00, 0x08 },
-		.max_mode         = 1,
-		.bl_state_address = 0xf3,
-		.state_base_value = 0x80,
-		.max_state        = 3,
-	},
-};
-
 static const char *ALLOWED_FW_39[] __initconst = {
 	"16R7IMS1.104", // Thin GF63 12HW
 	"16R8IMS1.101", // Thin GF63 12VE
@@ -4295,7 +4230,6 @@ static struct msi_ec_conf *CONFIGURATIONS[] __initdata = {
 	&CONF35,
 	&CONF36,
 	&CONF37,
-	&CONF38,
 	&CONF39,
 	&CONF40,
 	&CONF41,
