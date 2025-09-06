5fe672d4f90e0547095bd8396ed3b7d005fd3874
GlitchPunk <16731610+glpnk@users.noreply.github.com>
2025-09-06 04:57:26 +0300

    move 49 to 21

--- msi-ec.c
+++ msi-ec.c
@@ -1281,7 +1281,11 @@ static const char *ALLOWED_FW_21[] __initconst = {
 	"16R3EMS1.100", // GF63 Thin 9SC
 	"16R3EMS1.102",
 	"16R3EMS1.104",
+	"16R4EMS1.101", // GF63 Thin 10SCX(R) / 10SCS(R)
+	"16R4EMS1.102",
 	"16R4EMS2.102", // GF63 Thin 9SCSR
+	"16R5EMS1.101", // GF63 Thin 10U(C/D) / 10SC
+	"16R5EMS1.102",
 	NULL
 };
 
@@ -1320,6 +1324,7 @@ static struct msi_ec_conf CONF21 __initdata = {
 		.address = 0xf4,
 		.modes = {
 			{ FM_AUTO_NAME,     0x0d },
+			{ FM_SILENT_NAME,   0x1d },
 			{ FM_BASIC_NAME,    0x4d },
 			{ FM_ADVANCED_NAME, 0x8d },
 			MSI_EC_MODE_NULL
@@ -2850,77 +2855,6 @@ static struct msi_ec_conf CONF48 __initdata = {
 	},
 };
 
-static const char *ALLOWED_FW_49[] __initconst = {
-	"16R4EMS1.101", // GF63 Thin 10SCX(R) / 10SCS(R)
-	"16R4EMS1.102",
-	"16R5EMS1.101", // GF63 Thin 10U(C/D) / 10SC
-	"16R5EMS1.102",
-	NULL
-};
-
-static struct msi_ec_conf CONF49 __initdata = {
-	.allowed_fw = ALLOWED_FW_49, // WMI1 based
-	.charge_control_address = 0xef,
-	.webcam = {
-		.address       = 0x2e,
-		.block_address = 0x2f,
-		.bit           = 1,
-	},
-	.fn_win_swap = {
-		.address = 0xbf,
-		.bit     = 4,
-		.invert  = true,
-	},
-	.cooler_boost = {
-		.address = 0x98,
-		.bit     = 7,
-	},
-	.shift_mode = {
-		.address = 0xf2,
-		.modes = {
-			{ SM_ECO_NAME,     0xc2 },
-			{ SM_COMFORT_NAME, 0xc1 },
-			{ SM_SPORT_NAME,   0xc0 },
-			{ SM_TURBO_NAME,   0xc4 },
-			MSI_EC_MODE_NULL
-		},
-	},
-	.super_battery = {
-		.address = MSI_EC_ADDR_UNSUPP,
-		.mask    = 0x0f,
-	},
-	.fan_mode = {
-		.address = 0xf4,
-		.modes = {
-			{ FM_AUTO_NAME,     0x0d },
-			{ FM_SILENT_NAME,   0x1d },
-			{ FM_ADVANCED_NAME, 0x8d },
-			MSI_EC_MODE_NULL
-		},
-	},
-	.cpu = {
-		.rt_temp_address       = 0x68,
-		.rt_fan_speed_address  = 0x71,
-	},
-	.gpu = {
-		.rt_temp_address      = 0x80,
-		.rt_fan_speed_address = 0x89,
-	},
-	.leds = {
-		.micmute_led_address = MSI_EC_ADDR_UNSUPP,
-		.mute_led_address    = MSI_EC_ADDR_UNSUPP,
-		.bit                 = 1,
-	},
-	.kbd_bl = {
-		.bl_mode_address  = MSI_EC_ADDR_UNSUPP,
-		.bl_modes         = { 0x00, 0x08 },
-		.max_mode         = 1,
-		.bl_state_address = 0xf3,
-		.state_base_value = 0x80,
-		.max_state        = 3,
-	},
-};
-
 static const char *ALLOWED_FW_51[] __initconst = {
 	"158MEMS1.100", // Bravo 15 B5ED
 	"158MEMS1.101",
@@ -3502,7 +3436,6 @@ static struct msi_ec_conf *CONFIGURATIONS[] __initdata = {
 	&CONF46,
 	&CONF47,
 	&CONF48,
-	&CONF49,
 	&CONF51,
 	&CONF53,
 	&CONF54,
