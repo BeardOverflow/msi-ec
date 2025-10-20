75a8532a23563f00393d492ae2b80f671295b097
Fashim <fashim818@gmail.com>
2025-10-20 19:06:14 +1100

    add new conf G2_5 fw 14K1EMS1, 14K2EMS1

--- msi-ec.c
+++ msi-ec.c
@@ -1411,6 +1411,76 @@ static struct msi_ec_conf CONF_G2_4 __initdata = {
 	},
 };
 
+static const char *ALLOWED_FW_G2_5[] __initconst = {
+	"14K1EMS1.103", // Stealth 14 Studio A13VF
+	"14K1EMS1.108",
+	"14K2EMS1.104", // Stealth 14 AI Studio A1VGG / A1VFG
+	"14K2EMS1.107",
+	NULL
+};
+
+static struct msi_ec_conf CONF_G2_5 __initdata = {
+	.allowed_fw = ALLOWED_FW_G2_5, // new
+	.charge_control_address = 0xd7,
+	.webcam = {
+		.address       = 0x2e,
+		.block_address = 0x2f,
+		.bit           = 1,
+	},
+	.fn_win_swap = {
+		.address = 0xe8,
+		.bit     = 4,
+		.invert  = false,
+	},
+	.cooler_boost = {
+		.address = 0x98,
+		.bit     = 7,
+	},
+	.shift_mode = {
+		.address = 0xd2,
+		.modes = {
+			{ SM_ECO_NAME,     0xc2 },
+			{ SM_COMFORT_NAME, 0xc1 },
+			{ SM_TURBO_NAME,   0xc4 },
+			MSI_EC_MODE_NULL
+		},
+	},
+	.super_battery = {
+		.address = 0xeb,
+		.mask    = 0x0f,
+	},
+	.fan_mode = {
+		.address = 0xd4,
+		.modes = {
+			{ FM_AUTO_NAME,     0x0d },
+			{ FM_SILENT_NAME,   0x1d },
+			{ FM_ADVANCED_NAME, 0x8d },
+			MSI_EC_MODE_NULL
+		},
+	},
+	.cpu = {
+		.rt_temp_address      = 0x68,
+		.rt_fan_speed_address = 0x71,
+	},
+	.gpu = {
+		.rt_temp_address      = 0x80,
+		.rt_fan_speed_address = 0x89,
+	},
+	.leds = {
+		.micmute_led_address = 0x2c,
+		.mute_led_address    = 0x2d,
+		.bit                 = 1,
+	},
+	.kbd_bl = {
+		.bl_mode_address  = MSI_EC_ADDR_UNSUPP,
+		.bl_modes         = { 0x00, 0x08 },
+		.max_mode         = 1,
+		.bl_state_address = MSI_EC_ADDR_UNSUPP,
+		.state_base_value = 0x80,
+		.max_state        = 3,
+	},
+};
+
 static const char *ALLOWED_FW_G2_6[] __initconst = {
 	"16R6EMS1.104", // GF63 Thin 11UC / 11SC
 	"16R6EMS1.106",
@@ -1612,6 +1682,7 @@ static struct msi_ec_conf *CONFIGURATIONS[] __initdata = {
 	&CONF_G2_2,
 	&CONF_G2_3,
 	&CONF_G2_4,
+	&CONF_G2_5,
 	&CONF_G2_6,
 	&CONF_G2_10,
 	NULL
