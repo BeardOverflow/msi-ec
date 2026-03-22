167a6071e1cf888884bcf9e9696c76d760dad89f
Xabi Vittorio GOITY <xabigoity@gmail.com>
2026-03-22 21:16:55 +0100

    Changed approach to try to fix problems linked to scripts

--- packaging/nfpm-alpine-debian-slackware/scripts/preremove.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-SRC_DIR=$(ls -d /usr/src/msi-ec-* | sort -V | tail -n 1)
-VERSION_RELEASE=$(basename $SRC_DIR | sed 's/msi-ec-//')
-
-cd /usr/src/msi-ec-${VERSION_RELEASE}
-make dkms-uninstall
