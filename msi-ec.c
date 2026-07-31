b3f45a02eadaf172d96572034b12ffe5c4f6d671
Majd Abu Diab <majdabudiab2013@gmail.com>
2026-07-31 07:44:58 +0300

    Add new support for Summit E13 Flip Evo A13MT `13P3EMS1.508` (#695)
    Co-authored-by: GlitchPunk <16731610+glpnk@users.noreply.github.com>

--- msi-ec.c
+++ msi-ec.c
@@ -1344,6 +1344,7 @@ static const char *ALLOWED_FW_G2_3[] __initconst = {
 	"14N1EMS1.104", // Prestige 14 AI Evo C1MG
 	"14N1EMS1.307", // Prestige 14 AI Evo C2HMG
 	"13P3EMS1.107", // Summit E13 Flip A12MT
+	"13P3EMS1.508", // Summit E13 Flip Evo A13MT
 	"13P5EMS1.106", // Summit 13 AI+ Evo A2VM
 	"13Q2EMS1.110", // Prestige 13 AI Evo A1MG
 	"13Q3EMS1.111", // Prestige 13 AI+ Evo A2VMG
